e053e74a1d58d8553f6e906c1adacbd27ca8b4a1
NickHemerycke <safrynick@gmail.com>
2026-06-10 15:07:52 +0200

    setup the pom

---
+++ enterprise-apps/src/main/java/be/ehb/ngo/EnterpriseAppsApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.ngo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseAppsApplication.class, args);
+	}
+
+}
